9d9fd846635a9112e27b17d241998c9ff45fed88
andgeral <geraldinebecerra93@gmail.com>
2024-05-28 03:48:05 -0400

    se agrega ruta public

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(indexRoutes);
-
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
